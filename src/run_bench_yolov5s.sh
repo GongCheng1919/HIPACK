64bdd4eb646ff51c414d0d3d925e1f4eec9f62e9
junyi <hji200914@gmail.com>
2025-04-07 11:01:13 +0800

    fix

--- src/run_bench_yolov5s.sh
+++ src/run_bench_yolov5s.sh
@@ -14,28 +14,27 @@ WA_bits=(3 4)
 
 
 for i in {1,2,3,4,5}; do
-	for w_bit in "${WA_bits[@]}"; do
-		a_bit=${w_bit}
-		echo "config: W${w_bit}A${a_bit}"
-		LOG_FILE=logs/test_hipack_perf_${DEVICE}_${THREAD}_W${w_bit}A${a_bit}.log
-		mkdir -p logs
+	w_bit=3
+	a_bit=${w_bit}
+	echo "config: W${w_bit}A${a_bit}"
+	LOG_FILE=logs/test_hipack_perf_${DEVICE}_${THREAD}_W${w_bit}A${a_bit}.log
+	mkdir -p logs
 
-		echo "config: W${w_bit}A${a_bit}, save to: ${LOG_FILE}"
-		# ./${HIPACK_EXE} ${N} $((C)) $((H/2)) $((W/2)) $((C*2)) ${w_bit} ${a_bit} 0 0  | tee -a ${LOG_FILE}
-		./${HIPACK_EXE} 32 3 56 56 64 ${w_bit} ${a_bit} 0 0  | tee ${LOG_FILE}
-		./${HIPACK_EXE} 32 64 56 56 64 ${w_bit} ${a_bit} 0 0  | tee ${LOG_FILE}
-		./${HIPACK_EXE} 32 64 28 28 64 ${w_bit} ${a_bit} 0 0  | tee ${LOG_FILE}
-		./${HIPACK_EXE} 32 128 14 14 128 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
-		./${HIPACK_EXE} 32 256 7 7 256 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
-		./${HIPACK_EXE} 32 256 7 7 512 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
-		./${HIPACK_EXE} 32 512 7 7 512 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
-		./${HIPACK_EXE} 32 768 7 7 512 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
-		./${HIPACK_EXE} 32 512 14 14 256 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
-		./${HIPACK_EXE} 32 384 14 14 256 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
-		./${HIPACK_EXE} 32 256 14 14 256 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
-		./${HIPACK_EXE} 32 256 28 28 128 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
-		./${HIPACK_EXE} 32 192 28 28 128 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
-		./${HIPACK_EXE} 32 128 28 28 128 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
-		./${HIPACK_EXE} 32 128 56 56 64 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
-	done
+	echo "config: W${w_bit}A${a_bit}, save to: ${LOG_FILE}"
+	# ./${HIPACK_EXE} ${N} $((C)) $((H/2)) $((W/2)) $((C*2)) ${w_bit} ${a_bit} 0 0  | tee -a ${LOG_FILE}
+	./${HIPACK_EXE} 32 3 56 56 64 ${w_bit} ${a_bit} 0 0  | tee ${LOG_FILE}
+	./${HIPACK_EXE} 32 64 56 56 64 ${w_bit} ${a_bit} 0 0  | tee ${LOG_FILE}
+	./${HIPACK_EXE} 32 64 28 28 64 ${w_bit} ${a_bit} 0 0  | tee ${LOG_FILE}
+	./${HIPACK_EXE} 32 128 14 14 128 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
+	./${HIPACK_EXE} 32 256 7 7 256 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
+	./${HIPACK_EXE} 32 256 7 7 512 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
+	./${HIPACK_EXE} 32 512 7 7 512 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
+	./${HIPACK_EXE} 32 768 7 7 512 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
+	./${HIPACK_EXE} 32 512 14 14 256 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
+	./${HIPACK_EXE} 32 384 14 14 256 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
+	./${HIPACK_EXE} 32 256 14 14 256 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
+	./${HIPACK_EXE} 32 256 28 28 128 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
+	./${HIPACK_EXE} 32 192 28 28 128 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
+	./${HIPACK_EXE} 32 128 28 28 128 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
+	./${HIPACK_EXE} 32 128 56 56 64 ${w_bit} ${a_bit} 0 0 | tee ${LOG_FILE}
 done
