f31889a835d1759730b7ba436c3c5d3628674045
junyi <hji200914@gmail.com>
2025-04-08 14:54:09 +0800

    append log

--- torch_func/run_bench_diffusion.sh
+++ torch_func/run_bench_diffusion.sh
@@ -1,4 +1,4 @@
-DEVICE="rpi4_diffusion"
+DEVICE="rpi4_yolov5s"
 HIPACK_EXE=usage_of_directconv_args.py
 
 # Test the performance of the HIPACK
@@ -10,8 +10,8 @@ for i in $(seq 1 5); do
 	mkdir -p logs
 	echo "config: W${w_bit}A${a_bit}, save to: ${LOG_FILE}"
 
-	python3 ./${HIPACK_EXE} --N 32 --Ci 32 --H 160 --W 160 --Co 32 --W_bits ${w_bit} --A_bits ${a_bit}| tee ${LOG_FILE}
-	python3 ./${HIPACK_EXE} --N 32 --Ci 64 --H 80 --W 80 --Co 64 --W_bits ${w_bit} --A_bits ${a_bit}| tee ${LOG_FILE}
-	python3 ./${HIPACK_EXE} --N 32 --Ci 128 --H 40 --W 40 --Co 128 --W_bits ${w_bit} --A_bits ${a_bit}| tee ${LOG_FILE}
-	python3 ./${HIPACK_EXE} --N 32 --Ci 256 --H 20 --W 20 --Co 256 --W_bits ${w_bit} --A_bits ${a_bit}| tee ${LOG_FILE}
+	python3 ./${HIPACK_EXE} --N 32 --Ci 32 --H 160 --W 160 --Co 32 --W_bits ${w_bit} --A_bits ${a_bit}| tee -a ${LOG_FILE}
+	python3 ./${HIPACK_EXE} --N 32 --Ci 64 --H 80 --W 80 --Co 64 --W_bits ${w_bit} --A_bits ${a_bit}| tee -a ${LOG_FILE}
+	python3 ./${HIPACK_EXE} --N 32 --Ci 128 --H 40 --W 40 --Co 128 --W_bits ${w_bit} --A_bits ${a_bit}| tee -a ${LOG_FILE}
+	python3 ./${HIPACK_EXE} --N 32 --Ci 256 --H 20 --W 20 --Co 256 --W_bits ${w_bit} --A_bits ${a_bit}| tee -a ${LOG_FILE}
 done
--- torch_func/run_bench_yolov5s.sh
+++ torch_func/run_bench_yolov5s.sh
@@ -10,19 +10,19 @@ for i in $(seq 1 5); do
 	mkdir -p logs
 
 	echo "config: W${w_bit}A${a_bit}, save to: ${LOG_FILE}"
-	python3 ./${HIPACK_EXE} --N 32 --Ci 3 --H 56 --W 56 --Co 64 --W_bits ${w_bit} --A_bits ${a_bit}  | tee ${LOG_FILE}
-	python3 ./${HIPACK_EXE} --N 32 --Ci 64 --H 56 --W 56 --Co 64 --W_bits ${w_bit} --A_bits ${a_bit}  | tee ${LOG_FILE}
-	python3 ./${HIPACK_EXE} --N 32 --Ci 64 --H 28 --W 28 --Co 64 --W_bits ${w_bit} --A_bits ${a_bit}  | tee ${LOG_FILE}
-	python3 ./${HIPACK_EXE} --N 32 --Ci 128 --H 14 --W 14 --Co 128 --W_bits ${w_bit} --A_bits ${a_bit} | tee ${LOG_FILE}
-	python3 ./${HIPACK_EXE} --N 32 --Ci 256 --H 7 --W 7 --Co 256 --W_bits ${w_bit} --A_bits ${a_bit} | tee ${LOG_FILE}
-	python3 ./${HIPACK_EXE} --N 32 --Ci 256 --H 7 --W 7 --Co 512 --W_bits ${w_bit} --A_bits ${a_bit} | tee ${LOG_FILE}
-	python3 ./${HIPACK_EXE} --N 32 --Ci 512 --H 7 --W 7 --Co 512 --W_bits ${w_bit} --A_bits ${a_bit} | tee ${LOG_FILE}
-	python3 ./${HIPACK_EXE} --N 32 --Ci 768 --H 7 --W 7 --Co 512 --W_bits ${w_bit} --A_bits ${a_bit} | tee ${LOG_FILE}
-	python3 ./${HIPACK_EXE} --N 32 --Ci 512 --H 14 --W 14 --Co 256 --W_bits ${w_bit} --A_bits ${a_bit} | tee ${LOG_FILE}
-	python3 ./${HIPACK_EXE} --N 32 --Ci 384 --H 14 --W 14 --Co 256 --W_bits ${w_bit} --A_bits ${a_bit} | tee ${LOG_FILE}
-	python3 ./${HIPACK_EXE} --N 32 --Ci 256 --H 14 --W 14 --Co 256 --W_bits ${w_bit} --A_bits ${a_bit} | tee ${LOG_FILE}
-	python3 ./${HIPACK_EXE} --N 32 --Ci 256 --H 28 --W 28 --Co 128 --W_bits ${w_bit} --A_bits ${a_bit} | tee ${LOG_FILE}
-	python3 ./${HIPACK_EXE} --N 32 --Ci 192 --H 28 --W 28 --Co 128 --W_bits ${w_bit} --A_bits ${a_bit} | tee ${LOG_FILE}
-	python3 ./${HIPACK_EXE} --N 32 --Ci 128 --H 28 --W 28 --Co 128 --W_bits ${w_bit} --A_bits ${a_bit} | tee ${LOG_FILE}
-	python3 ./${HIPACK_EXE} --N 32 --Ci 128 --H 56 --W 56 --Co 64 --W_bits ${w_bit} --A_bits ${a_bit} | tee ${LOG_FILE}
+	python3 ./${HIPACK_EXE} --N 32 --Ci 3 --H 56 --W 56 --Co 64 --W_bits ${w_bit} --A_bits ${a_bit}  | tee -a ${LOG_FILE}
+	python3 ./${HIPACK_EXE} --N 32 --Ci 64 --H 56 --W 56 --Co 64 --W_bits ${w_bit} --A_bits ${a_bit}  | tee -a ${LOG_FILE}
+	python3 ./${HIPACK_EXE} --N 32 --Ci 64 --H 28 --W 28 --Co 64 --W_bits ${w_bit} --A_bits ${a_bit}  | tee -a ${LOG_FILE}
+	python3 ./${HIPACK_EXE} --N 32 --Ci 128 --H 14 --W 14 --Co 128 --W_bits ${w_bit} --A_bits ${a_bit} | tee -a ${LOG_FILE}
+	python3 ./${HIPACK_EXE} --N 32 --Ci 256 --H 7 --W 7 --Co 256 --W_bits ${w_bit} --A_bits ${a_bit} | tee -a ${LOG_FILE}
+	python3 ./${HIPACK_EXE} --N 32 --Ci 256 --H 7 --W 7 --Co 512 --W_bits ${w_bit} --A_bits ${a_bit} | tee -a ${LOG_FILE}
+	python3 ./${HIPACK_EXE} --N 32 --Ci 512 --H 7 --W 7 --Co 512 --W_bits ${w_bit} --A_bits ${a_bit} | tee -a ${LOG_FILE}
+	python3 ./${HIPACK_EXE} --N 32 --Ci 768 --H 7 --W 7 --Co 512 --W_bits ${w_bit} --A_bits ${a_bit} | tee -a ${LOG_FILE}
+	python3 ./${HIPACK_EXE} --N 32 --Ci 512 --H 14 --W 14 --Co 256 --W_bits ${w_bit} --A_bits ${a_bit} | tee -a ${LOG_FILE}
+	python3 ./${HIPACK_EXE} --N 32 --Ci 384 --H 14 --W 14 --Co 256 --W_bits ${w_bit} --A_bits ${a_bit} | tee -a ${LOG_FILE}
+	python3 ./${HIPACK_EXE} --N 32 --Ci 256 --H 14 --W 14 --Co 256 --W_bits ${w_bit} --A_bits ${a_bit} | tee -a ${LOG_FILE}
+	python3 ./${HIPACK_EXE} --N 32 --Ci 256 --H 28 --W 28 --Co 128 --W_bits ${w_bit} --A_bits ${a_bit} | tee -a ${LOG_FILE}
+	python3 ./${HIPACK_EXE} --N 32 --Ci 192 --H 28 --W 28 --Co 128 --W_bits ${w_bit} --A_bits ${a_bit} | tee -a ${LOG_FILE}
+	python3 ./${HIPACK_EXE} --N 32 --Ci 128 --H 28 --W 28 --Co 128 --W_bits ${w_bit} --A_bits ${a_bit} | tee -a ${LOG_FILE}
+	python3 ./${HIPACK_EXE} --N 32 --Ci 128 --H 56 --W 56 --Co 64 --W_bits ${w_bit} --A_bits ${a_bit} | tee -a ${LOG_FILE}
 done
